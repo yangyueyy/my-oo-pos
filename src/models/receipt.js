783626949a51b47a1d845396b32caa7f85c882e0
yangyue <m18710434142_1@163.com>
2016-07-26 01:00:30 +0800

    buildReceiptText function to receipt.buildText methed

--- src/models/receipt.js
+++ src/models/receipt.js
@@ -1,23 +1,47 @@
-class Receipt{
+class Receipt {
 
-    constructor(receiptItems,savedTotal,total){
+    constructor(receiptItems, savedTotal, total) {
         this.receiptItems = receiptItems;
         this.savedTotal = savedTotal;
         this.total = total;
     }
 
-    static buildReceipt (receiptItems) {
+    buildText() {
+        const formatMoney = (money) => {
+            return money.toFixed(2);
+        };
 
-    let total = 0;
-    let savedTotal = 0;
+        let receiptItemsText = this.receiptItems
+            .map(receiptItem => {
+                const cartItem = receiptItem.cartItem;
+                return `名称：${cartItem.item.name}，\
+数量：${cartItem.count}${cartItem.item.unit}，\
+单价：${formatMoney(cartItem.item.price)}(元)，\
+小计：${formatMoney(receiptItem.subtotal)}(元)`;
+            })
+            .join('\n');
 
-    for (const receiptItem of receiptItems) {
-        total += receiptItem.subtotal;
-        savedTotal += receiptItem.saved;
+        return `***<没钱赚商店>收据***
+${receiptItemsText}
+----------------------
+总计：${formatMoney(this.total)}(元)
+节省：${formatMoney(this.savedTotal)}(元)
+**********************`;
     }
 
-    return new Receipt(receiptItems,savedTotal,total);
-}
+
+    static buildReceipt(receiptItems) {
+
+        let total = 0;
+        let savedTotal = 0;
+
+        for (const receiptItem of receiptItems) {
+            total += receiptItem.subtotal;
+            savedTotal += receiptItem.saved;
+        }
+
+        return new Receipt(receiptItems, savedTotal, total);
+    }
 }
 
 module.exports = Receipt;
