f801f14914d1f6c90b8d6be1a7a43c5838814a90
yangyue <m18710434142_1@163.com>
2016-07-26 00:06:56 +0800

    create Receipt class

---
+++ src/models/receipt.js
@@ -0,0 +1,10 @@
+class Receipt{
+
+    constructor(receiptItems,savedTotal,total){
+        this.receiptItems = receiptItems;
+        this.savedTotal = savedTotal;
+        this.total = total;
+    }
+}
+
+mudule.exports = Receipt;
