9a5386deea05e04461f8c7ada33cfdabb9ffd7e7
yangyue <m18710434142_1@163.com>
2016-07-26 00:33:43 +0800

    buildCartItems function to CartItem.buildCartItems methed

--- src/models/cart-item.js
+++ src/models/cart-item.js
@@ -1,9 +1,33 @@
-class CartItem{
+class CartItem {
 
-    constructor(item,count=1){
+    constructor(item, count = 1) {
         this.item = item;
         this.count = count;
     }
+
+    static buildCartItems(tags, allItems) {
+
+        const cartItems = [];
+
+        for (const tag of tags) {
+
+            const tagArray = tag.split('-');
+            const barcode = tagArray[0];
+            const count = parseFloat(tagArray[1] || 1);
+
+            const cartItem = cartItems.find(cartItem => cartItem.item.barcode === barcode);
+
+            if (cartItem) {
+                cartItem.count += count;
+            } else {
+                const item = allItems.find(item => item.barcode === barcode);
+                cartItems.push(new CartItem(item, count));
+            }
+        }
+
+        return cartItems;
+    }
+
 }
 
 module.exports = CartItem;
